43430faf4121251b7c9d7524478a5455fcec572d
Mehedi Hasan Shuvo <67133203+duetianmehedishuvo@users.noreply.github.com>
2024-03-17 17:12:43 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:women_safety/provider/auth_provider.dart';
+import 'package:women_safety/screen/splash_screen.dart';
+import 'package:women_safety/util/helper.dart';
+import 'di_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyAGDClS7C4rYoVs77kDlcs5gPLPL-_7nNw",
+      appId: "1:25337127069:android:df4d0b0c79625c4f447474",
+      messagingSenderId: "25337127069",
+      projectId: "solar-power-aerator-for-79b02",
+    ),
+  );
+
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => di.sl<AuthProvider>()),
+    ],
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Load Control',
+      navigatorKey: Helper.navigatorKey,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/screen/dashboard/dashboard_screen.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:women_safety/helper/message_dao.dart';
+import 'package:women_safety/util/size.util.dart';
+import 'package:women_safety/util/theme/app_colors.dart';
+import 'package:women_safety/util/theme/text.styles.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  List<bool> allStatus = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //     title: Text('Load Control ', style: sfProStyle400Regular.copyWith(fontSize: 14, color: Colors.white)),
+      //     centerTitle: true,
+      //     backgroundColor: colorPrimary),
+      body: SafeArea(
+        child: StreamBuilder(
+            stream: MessageDao.messagesRef.onValue,
+            builder: (c, snapshot) {
+              if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              } else if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              snapshot.data!.snapshot.child('INTACT_POWER').children.forEach((element) {
+                int position = int.parse(element.key!.replaceAll('device_', ''))-1;
+                bool value = int.parse(element.value.toString()) == 1;
+                allStatus[position] = value;
+              });
+
+              return Builder(builder: (context) {
+                return Column(
+                  children: [
+                    Container(
+                      width: getAppSizeWidth(),
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: const BoxDecoration(
+                          color: colorPrimary, borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15))),
+                      child: Text('Welcome, Load Control Systems', style: sfProStyle500Medium.copyWith(fontSize: 15, color: Colors.white)),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: 25,
+                        physics: const BouncingScrollPhysics(),
+                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                        itemBuilder: (context,index){
+                          return buildEachRow(allStatus[index], 'device_${index+1}',index);
+                        },
+
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Center(child: Text("Developed by Mehedi Hasan Shuvo.", style: sfProStyle400Regular.copyWith(fontSize: 16))),
+                    const SizedBox(height: 20),
+                  ],
+                );
+              });
+            }),
+      ),
+    );
+  }
+
+  Widget buildEachRow(bool status, String path,int index) {
+    return eachRow(
+      "Device ${index>8?'':'0'}${index+1}",
+      "OFF",
+      widget: Container(
+        height: 18,
+        width: 20,
+        alignment: Alignment.centerRight,
+        child: Switch(
+            onChanged: (bool value) {
+              MessageDao.messagesRef.ref.child("INTACT_POWER").update({path: status ? 0 : 1});
+            },
+            value: status,
+            activeColor: Colors.white,
+            activeTrackColor: colorPrimary),
+      ),
+    );
+  }
+
+  Widget eachRow(String key, String value, {Widget? widget}) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(border: Border.all(color: colorIcons.withOpacity(.6)), borderRadius: BorderRadius.circular(4)),
+      child: IntrinsicHeight(
+        child: Row(
+          children: [
+            Expanded(flex: 2, child: Text(key, style: sfProStyle600SemiBold.copyWith(fontSize: 14, color: Colors.black))),
+            VerticalDivider(color: colorIcons.withOpacity(.6), thickness: 1.1),
+            Expanded(child: widget ?? Text(value, textAlign: TextAlign.end, style: sfProStyle400Regular.copyWith(fontSize: 16, color: Colors.black))),
+          ],
+        ),
+      ),
+    );
+  }
+}
